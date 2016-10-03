68a728886aff3cb3b37273acb1e4daed227dff41
tin-wong <tin.h.wong@gmail.com>
2016-10-03 16:45:38 -0700

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -8,10 +8,6 @@ exports.config = {
     // should work too though). These services define specific user and key (or access key)
     // values you need to put in here in order to connect to these services.
     //
-    user: process.env.SAUCE_USERNAME,
-    key: process.env.SAUCE_ACCESS_KEY,
-    
-    
     //
     // ==================
     // Specify Test Files
